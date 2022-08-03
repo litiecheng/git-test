87f10123d9e6c4b70db74fe45c333bf15a89593b
litiecheng <litiechengstart@163.com>
2022-08-03 20:34:29 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author ltcstart
+ * @date 2022/8/3 - 20:32
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
